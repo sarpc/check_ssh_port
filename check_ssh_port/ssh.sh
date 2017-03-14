536beaf69822547b7cf98ec24705fdb945c7e356
sarpc <sarpc@yandex.ru>
2017-03-14 18:50:26 +0300

    Добавил описание строк

--- check_ssh_port/ssh.sh
+++ check_ssh_port/ssh.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#
-# cat cron_add_2h_ssh.sh >> /etc/cron.d/dailyjobs
-#
+# В Cron проверка каждые два часа
+cat cron_add_2h_ssh.sh >> /etc/cron.d/dailyjobs
 
 
+# Генерация ключей
 echo "Generating SSH key..."
 if [ ! -e ~/.ssh/id_rsa ]
  then
     ssh-keygen -t rsa  -f ~/.ssh/id_rsa -P ""
-
+# Отправка на emai публичного ключа.
     echo "Public files to email..." | mutt -s "Public key" sarpc@ya.ru -a /root/.ssh/id_rsa.pub
 fi
